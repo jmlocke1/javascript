d72a0c611a97b8e8cc2db36fd35ecdc6b6927f5d
Josemi Xeon <worldcode.info@gmail.com>
2022-09-10 04:35:23 +0200

    Terminado el CRUD con Fetch asíncrono

--- jonmircha/ajax/js/crud-fetch.js
+++ jonmircha/ajax/js/crud-fetch.js
@@ -6,6 +6,7 @@ const d = document,
 	$fragment = d.createDocumentFragment();
 
 function cleanAll(){
+	if($form.nombre.value) console.log("Voy a limpiar el formulario");
 	// Limpiamos el formulario
 	$form.nombre.value = "";
 	$form.constelacion.value = "";
@@ -14,6 +15,7 @@ function cleanAll(){
 	$title.textContent = "Agregar Santo";
 	// Borramos la tabla anterior
 	$table.querySelector("tbody").innerHTML = "";
+	
 }
 
 const getAll = async () => {
@@ -24,7 +26,6 @@ const getAll = async () => {
 
 		if(!res.ok) throw { status: res.status, statusText: res.statusText };
 
-		console.log(json);
 		json.forEach( el => {
 			$template.querySelector(".name").textContent = el.nombre;
 			$template.querySelector(".constellation").textContent = el.constelacion;
@@ -46,3 +47,66 @@ const getAll = async () => {
 }
 
 d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", async e => {
+	if(e.target !== $form){
+		return;
+	}
+	e.preventDefault();
+	let data = {
+		nombre: e.target.nombre.value,
+		constelacion: e.target.constelacion.value
+	},
+		method = '';
+		url = 'http://localhost:5000/santos';
+	if(!e.target.id.value){
+		// Create - POST
+		method = 'POST';
+	} else {
+		// Update - PUT
+		url += `/${e.target.id.value}`;
+		method = 'PUT';
+	}
+	try {
+		let res = await fetch(url, {
+			method: method,
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8'
+			},
+			body: JSON.stringify(data)
+		});
+		getAll();
+		if(!res.ok) throw { status: res.status, statusText: res.statusText};
+	} catch (err) {
+		let message = err.statusText || "Ocurrió un error";
+		$form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+	}
+});
+
+d.addEventListener("click", async e => {
+	if(e.target.matches(".edit")) {
+		$title.textContent = "Editar Santo";
+		$form.nombre.value = e.target.dataset.name;
+		$form.constelacion.value = e.target.dataset.constellation;
+		$form.id.value = e.target.dataset.id;
+	}
+
+	if(e.target.matches(".delete")){
+		let isDelete = confirm(`¿Estás seguro de querer eliminar el caballero ${e.target.dataset.name} de la constelación ${e.target.dataset.constellation}?`);
+		if(!isDelete) return;
+		try {
+			let res = await fetch(`http://localhost:5000/santos/${e.target.dataset.id}`, {
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			getAll();
+			if(!res.ok) throw { status: res.status, statusText: res.statusText};
+		} catch (err) {
+			let message = err.statusText || "Ocurrió un error";
+			$form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+		}
+	}
+});
+
